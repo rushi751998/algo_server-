a75a297c4715a31ff9f358ce8c6e98f49bdc47e9
Rushikesh Shivaji More <67890585+rushi751998@users.noreply.github.com>
2024-10-18 09:36:19 +0530

    Update order_management.py

--- server/order_management.py
+++ server/order_management.py
@@ -542,7 +542,7 @@ class Order_management :
                 folder_path = "server/plots/Available_Margin"
                 
             os.makedirs(folder_path, exist_ok=True)
-            image_full_path = f'{folder_path}/{self.date}}.png'
+            image_full_path = f'{folder_path}/{self.date}}.png}'
             fig.write_image(image_full_path)
             send_message(message = image_full_path,send_image=True)
             
